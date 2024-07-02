394c9d2c03c8b3deaae11a2b7e9987a1c67cb81a
Obiefuna Kingsley <kingsleyobiefuna01@gmail.com>
2024-07-02 22:48:17 +0200

    implemented toggle for the form and closing the form with or without completion

--- script.js
+++ script.js
@@ -16,3 +16,31 @@ function Book(title, author, pages, read) {
 const harryPotter = new Book("Harry Potter", "J.K Rowling", 3407, true);
 const aSongOfIceAndFire = new Book("A Song Of Ice And Fire", "George R.R Martin", 5178, true);
 const ninteenEightyFour = new Book("1984", "George Orwell", 394, false);
+
+const new_book_button = document.querySelector("header button"),
+      blanket = document.querySelector(".blanket"),
+      books = document.querySelector(".books"),
+      body = document.querySelector("body"),
+      form = document.querySelector("form"),
+      close_button = document.querySelector("form svg");
+
+new_book_button.addEventListener("click", () => {    
+    toggleProperties();
+});
+
+close_button.addEventListener("click", () => {
+    toggleProperties();
+})
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+
+    toggleProperties();
+})
+
+function toggleProperties() {
+    blanket.classList.toggle("show");
+    form.classList.toggle("show");
+    body.classList.toggle("no-overflow");
+}
